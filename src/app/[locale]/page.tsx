09f4a6147bc2efc07f8e379a577b891aef2b788e
MargusS <agusnicolas21@gmail.com>
2025-08-31 17:16:39 +0100

    Add i18n configuration

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,7 +1,20 @@
+import { use } from 'react';
+import { useTranslations } from 'next-intl';
+import { setRequestLocale } from 'next-intl/server';
+import { Link } from '@/i18n/navigation';
+import { routing } from '@/i18n/routing';
+export default function Home({ params }: { params: Promise<{ locale: string }> }) {
 
-export default function Home() {
+	const { locale } = use(params);
+
+	// Requisito para SSG con hooks de next-intl
+	setRequestLocale(locale);
+
+	const dictonary = useTranslations('HomePage');
+
 	return (
 		<main className="p-8 space-y-6">
+			<h1 className="text-3xl font-bold">{dictonary('title')}</h1>
 			<h1 className="text-3xl font-bold">Nimbus Sans Heading</h1>
 			<p>
 				This paragraph uses <span className="font-sans">Nimbus Sans</span> as the default body font.
