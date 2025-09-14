236d3fe060c68ccc2a80e55bc1db841a55030c4e
MargusS <agusnicolas21@gmail.com>
2025-09-14 21:24:07 +0100

    Responsive MainAceroBottom

--- src/app/components/MainAceroBottom.tsx
+++ src/app/components/MainAceroBottom.tsx
@@ -2,14 +2,15 @@ import Image from 'next/image';
 export default function MainAceroBottom() {
 
 	return (
-		<div className="absolute left-1/2 transform -translate-x-1/2 bottom-10 w-[85vw] max-w-2xl">
+		<div className="self-end col-start-1 row-start-6 md:row-start-8 col-span-3">
 			<Image
 				src="/logos/main-white.png"
 				alt="Pilates Studio Dot Matrix"
-				width={320}
-				height={60}
-				style={{ width: "100%", height: "auto" }}
+				width={1280}
+				height={240}
+				quality={100}
 				priority
+				className="w-[80vw] max-w-lg md:max-w-[600px] 2xl:max-w-[1000px]"
 			/>
 		</div>
 	);
