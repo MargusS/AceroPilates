5341727b9a72acc4d0e9a60d1aa1216f6b2e1474
MargusS <agusnicolas21@gmail.com>
2025-09-14 22:00:23 +0100

    Home Section v1

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,38 +1,22 @@
-import { use } from 'react';
 import { useTranslations } from 'next-intl';
 import { setRequestLocale } from 'next-intl/server';
-import { Link } from '@/i18n/navigation';
-import { routing } from '@/i18n/routing';
+import { use } from 'react';
+import FullScreenSection from '../components/shared/FullScreenSection';
+import HomeSection from '../components/HomeSection';
+import Navbar from '../components/Navbar';
+import SpaceSection from '../components/SpaceSection';
 export default function Home({ params }: { params: Promise<{ locale: string }> }) {
 
 	const { locale } = use(params);
 
-	// Requisito para SSG con hooks de next-intl
 	setRequestLocale(locale);
-
-	const dictonary = useTranslations('HomePage');
+	const dictonary = useTranslations('LandingPage');
 
 	return (
-		<main className="p-8 space-y-6">
-			<h1 className="text-3xl font-bold">{dictonary('title')}</h1>
-			<h1 className="text-3xl font-bold">Nimbus Sans Heading</h1>
-			<p>
-				This paragraph uses <span className="font-sans">Nimbus Sans</span> as the default body font.
-			</p>
-
-			<div>
-				<h2 className="font-dotmatrix font-bold text-2xl mb-2">DotMatrix Title</h2>
-				<p className="font-dotmatrix text-lg">
-					This line is rendered in the DotMatrix custom font for a digital or retro look.
-				</p>
-			</div>
-
-			<div>
-				<h2 className="font-tiny font-bold text-2xl mb-2">Tiny Title</h2>
-				<p className="font-tiny text-lg">
-					This line is rendered in the DotMatrix custom font for a digital or retro look.
-				</p>
-			</div>
+		<main className="">
+			<Navbar />
+			<FullScreenSection bgImage="/images/image00000.jpg" imagePosition="center"><HomeSection /></FullScreenSection>
+			<FullScreenSection bgImage="/images/image00004.jpg" imagePosition="right"><SpaceSection /></FullScreenSection>
 		</main>
 	);
 }
