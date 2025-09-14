1a98a688eff0c4385c8f9c31accaf05290a8f4a8
MargusS <agusnicolas21@gmail.com>
2025-09-14 17:57:33 +0100

    Add Address Component

---
+++ src/app/components/Address.tsx
@@ -0,0 +1,23 @@
+
+export default function Address() {
+
+	return (
+		<div>
+			<span className="font-medium text-[8px]/[16px] tracking-[.25em]">602 - 721 - 291</span>
+
+			<div className='flex gap-5'>
+				<span className="font-light tracking-widest">
+					CALLE CAMINO VIEJO
+				</span>
+				<span className="tracking-[1em]">MADRID</span>
+			</div>
+			<div className='flex gap-1 tracking-[0.5em]'>
+				<span className="font-light">LEGANES</span>
+				<span className="font-medium">23</span>
+				<span className="font-light">BIS</span>
+				<span className="font-medium tracking-widest">METRO URGEL</span>
+			</div>
+		</div>
+
+	);
+}
