752b59948cf3d8c90082e961936c0e9e69f1fe2b
MargusS <agusnicolas21@gmail.com>
2025-09-14 18:23:30 +0100

    Change nav sttyle

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -2,52 +2,52 @@
 import { useEffect, useState } from "react";
 
 export default function ScrollNav() {
-  const navHeight = 20;
-  const offsetTop = 40;
-  const [isFixed, setIsFixed] = useState(false);
-  const [navReady, setNavReady] = useState(false); // Control renderizado del nav
-  const [centerTop, setCenterTop] = useState<number>(0);
+	const navHeight = 0;
+	const offsetTop = 40;
+	const [isFixed, setIsFixed] = useState(false);
+	const [navReady, setNavReady] = useState(false); // Control renderizado del nav
+	const [centerTop, setCenterTop] = useState<number>(0);
 
-  useEffect(() => {
-    // Solo ejecuta en el cliente
-    const calcCenter = () => {
-      setCenterTop(window.innerHeight / 2 - navHeight / 2);
-      setNavReady(true);
-    };
-    calcCenter();
-    window.addEventListener("resize", calcCenter);
+	useEffect(() => {
+		// Solo ejecuta en el cliente
+		const calcCenter = () => {
+			setCenterTop(window.innerHeight / 2 - navHeight / 2);
+			setNavReady(true);
+		};
+		calcCenter();
+		window.addEventListener("resize", calcCenter);
 
-    const handleScroll = () => {
-      const scrollY = window.scrollY;
-      if (scrollY >= centerTop - offsetTop) {
-        setIsFixed(true);
-      } else {
-        setIsFixed(false);
-      }
-    };
-    window.addEventListener("scroll", handleScroll);
+		const handleScroll = () => {
+			const scrollY = window.scrollY;
+			if (scrollY >= centerTop - offsetTop) {
+				setIsFixed(true);
+			} else {
+				setIsFixed(false);
+			}
+		};
+		window.addEventListener("scroll", handleScroll);
 
-    return () => {
-      window.removeEventListener("resize", calcCenter);
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, [centerTop, navHeight, offsetTop]);
+		return () => {
+			window.removeEventListener("resize", calcCenter);
+			window.removeEventListener("scroll", handleScroll);
+		};
+	}, [centerTop, navHeight, offsetTop]);
 
-  // Renderiza nav solo cuando se conoce el valor real del top
-  if (!navReady) return null; // Alternativamente, un loader o un espacio vacío
+	// Renderiza nav solo cuando se conoce el valor real del top
+	if (!navReady) return null; // Alternativamente, un loader o un espacio vacío
+	const positionClass = isFixed ? "fixed" : "absolute";
 
-  return (
-    <nav
-      className={isFixed
-        ? "fixed w-full left-1/2 transform -translate-x-1/2 bg-white/40 backdrop-blur-sm text-white font-medium text-sm flex justify-around items-center py-1 z-50"
-        : "absolute w-full left-1/2 transform -translate-x-1/2 bg-white/40 backdrop-blur-sm text-white font-medium text-sm flex justify-around items-center py-1 z-50"}
-      style={{ top: isFixed ? offsetTop : centerTop }}
-    >
-      <a href="#" className="px-1 hover:text-gray-300 transition text-xs">ESPACIO</a>
-      <a href="#" className="px-1 hover:text-gray-300 transition text-xs">EQUIPO</a>
-      <a href="#" className="px-1 hover:text-gray-300 transition text-xs">SERVICIOS</a>
-      <a href="#" className="px-1 hover:text-gray-300 transition text-xs">TARIFAS</a>
-      <a href="#" className="px-1 hover:text-gray-300 transition text-xs">CONTACTO</a>
-    </nav>
-  );
+	return (
+		<nav
+			className={`${positionClass} w-full left-1/2 transform -translate-x-1/2 bg-white/90 backdrop-blur-sm text-white font-medium text-sm flex justify-center items-center gap-7 py-1 z-50`}
+
+			style={{ top: isFixed ? offsetTop : centerTop }}
+		>
+			<a href="#" className=" text-metal-300 hover:text-gray-300 transition text-[9px]/[9px] md:text-xs font-bold">EQUIPO</a>
+			<a href="#" className=" text-metal-300 hover:text-gray-300 transition text-[9px]/[9px] md:text-xs font-bold">ESPACIO</a>
+			<a href="#" className=" text-metal-300 hover:text-gray-300 transition text-[9px]/[9px] md:text-xs font-bold">SERVICIOS</a>
+			<a href="#" className=" text-metal-300 hover:text-gray-300 transition text-[9px]/[9px] md:text-xs font-bold">TARIFAS</a>
+			<a href="#" className=" text-metal-300 hover:text-gray-300 transition text-[9px]/[9px] md:text-xs font-bold">CONTACTO</a>
+		</nav>
+	);
 }
