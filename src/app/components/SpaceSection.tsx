4a2dab29775d516b0e91da9b165aee1746b47266
MargusS <agusnicolas21@gmail.com>
2025-09-15 18:04:13 +0100

    Add Header to SpaceSection

--- src/app/components/SpaceSection.tsx
+++ src/app/components/SpaceSection.tsx
@@ -5,8 +5,8 @@ export default function SpaceSection() {
 	return (
 		<div className="relative z-20 h-full w-full px-4 grid grid-flow-col grid-rows-2 md:grid-rows-6">
 			<div className='md:row-span-3 '>
-				<MainAcero imageOrientation="rotate-x-180" imagePosition="self-start" rowStart={1} />
-				<MainAcero imageOrientation="opacity-50" imagePosition="self-start" rowStart={2} />
+				<MainAcero imageLayout="rotate-x-180 self-start row-start-2 md:row-start-1" />
+				<MainAcero imageLayout="opacity-50 self-start row-start-2 md:row-start-4" />
 			</div>
 
 
