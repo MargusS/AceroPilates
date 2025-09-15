9ff162d8cd1956f6c2cb4ba61f091e660728e692
MargusS <agusnicolas21@gmail.com>
2025-09-15 19:27:16 +0100

    Make Instagram Fixed

--- src/app/components/shared/Instagram.tsx
+++ src/app/components/shared/Instagram.tsx
@@ -3,16 +3,16 @@ import Link from 'next/link';
 export default function Instagram() {
 
 	return (
-		<div className="flex items-center">
-			<Link href="https://instagram.com/aceropilates/" target="_blank" rel="noopener noreferrer" className="flex gap-2 items-center rotate-90">
-				<Image
-					src="/icons/instagram.svg"
-					alt="Instagram"
-					width={30}
-					height={30}
-				/>
-				<span className="font-semibold text-white text-[14px] 2xl:text-lg tracking-[.6em]">aceropilates</span>
-			</Link>
+		<div className="fixed bottom-1/4 right-[-4rem] md:right-[-3rem] z-50 ">
+				<Link href="https://instagram.com/aceropilates/" target="_blank" rel="noopener noreferrer" className="flex gap-2 items-center rotate-90">
+					<Image
+						src="/icons/instagram.svg"
+						alt="Instagram"
+						width={30}
+						height={30}
+					/>
+					<h4 className="font-semibold text-white text-[14px] 2xl:text-lg tracking-[.6em] ">aceropilates</h4>
+				</Link>
 		</div>
 	);
 }
