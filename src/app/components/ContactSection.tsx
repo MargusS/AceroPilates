e664793f5ac00287fa27f5967ac8a81b70dacb1d
MargusS <agusnicolas21@gmail.com>
2025-09-28 15:46:15 +0100

    New Layout

--- src/app/components/ContactSection.tsx
+++ src/app/components/ContactSection.tsx
@@ -8,22 +8,12 @@ export default function ContactSection() {
 	return (
 		<div className="relative z-20 h-full w-full px-4 grid grid-flow-col grid-rows-2 md:grid-rows-6 overflow-hidden">
 
-			<div className="my-2 md:row-span-3 place-self-start w-full flex flex-col md:flex-row md:justify-center items-center gap-12 md:gap-25">
-				<Image
-					src="/logos/dotted-white.png"
-					alt="Pilates Studio Dot Matrix"
-					width={1280}
-					height={240}
-					className="w-[70vw] max-w-lg md:w-[40vw] md:max-w-[550px]"
-				/>
-			</div>
-
-			<div className='absolute top-[25%] xl:top-[33%] h-[32vh] xl:h-[55vh] w-full xl:w-[40vw] xl:max-w-[600px] xl:right-1/2 xl:translate-x-1/2 z-10 '>
+			<div className='absolute top-[22%] h-[32vh] xl:h-[80vh] w-full xl:w-[70vw] xl:right-0 xl:opacity-80 z-0'>
 				<LocationMap />
 			</div>
 
-			<div className="md:row-span-3 grid grid-rows-6 md:grid-rows-8 grid-cols-3 justify-items-center w-full">
-				<div className="row-start-2 xl:row-start-1 col-span-3 self-end xl:self-start place-self-start pl-5 md:pl-15">
+			<div className="row-start-2 md:row-start-4 md:row-span-3 grid grid-rows-6 md:grid-rows-8 grid-cols-3 justify-items-center w-full">
+				<div className="row-start-2 md:row-start-3 col-span-3 self-end xl:self-start place-self-start pl-5 md:pl-15">
 					{/* <p className="text-white text-[11px]/[12px] md:text-[11px]/[14px] 2xl:text-[13px]/[16px] font-normal tracking-[1.5em]">HORARIOS</p> */}
 					<div className='flex flex-col mt-3 md:mt-10 xl:mt-0 text-white font-light leading-5 text-sm md:text-lg 2xl:text-xl'>
 						<div className='flex align-center gap-2 md:gap-4'>
@@ -46,7 +36,7 @@ export default function ContactSection() {
 				<div className="row-start-5 md:row-start-6 col-span-2 self-start md:self-end place-self-start pl-5 md:pl-15">
 					<Email />
 				</div>
-				<div className='row-start-6 md:row-start-8 col-span-3 self-center flex justify-center items-center text-center gap-4 md:gap-12 text-white text-[8px]/[12px] 2xl:text-sm tracking-widest font-light'>
+				{/* <div className='row-start-6 md:row-start-8 col-span-3 self-center flex justify-center items-center text-center gap-4 md:gap-12 text-white text-[8px]/[12px] 2xl:text-sm tracking-widest font-light'>
 					<span>PILATES</span>
 					<span>SUELO</span>
 					<div className='flex flex-col'>
@@ -55,7 +45,7 @@ export default function ContactSection() {
 					</div>
 					<span>PILATES</span>
 					<span>EMBARAZO</span>
-				</div>
+				</div> */}
 				{/* <MainAcero imageLayout="row-start-6 md:row-start-8 self-end" /> */}
 			</div>
 
--- src/app/components/ServicesSection.tsx
+++ src/app/components/ServicesSection.tsx
@@ -6,10 +6,15 @@ export default function ServicesSection() {
 	const items = ["INICIAL", "INTERMEDIO", "AVANZADO"];
 
 	return (
-
-		<div className="relative h-full w-full grid grid-flow-col grid-rows-3">
-			<div className="relative h-full w-full grid grid-flow-col grid-rows-3">
-				<div className="row-start-3 place-self-center lg:place-self-end relative no-scrollbar flex overflow-x-hidden w-full items-center">
+		<section id="services" className="relative h-full w-full grid grid-flow-col grid-rows-[20vh_10vh_55vh_15vh] z-20">
+			<div className='row-start-1'>
+				{/* <MainAcero imageLayout="rotate-x-180 self-start" />
+							<MainAcero imageLayout="opacity-50 self-end" /> */}
+				<MainAcero imageLayout="rotate-x-180 opacity-50 self-start" />
+				<MainAcero imageLayout=" self-end" />
+			</div>
+			<div className="row-start-2 relative h-full w-full grid grid-flow-col ">
+				<div className="place-self-center lg:place-self-end relative no-scrollbar flex overflow-x-hidden w-full items-center">
 					<div className="animate-marquee flex whitespace-nowrap md:justify-around md:w-full">
 						{items.map((item, i) => (
 							<span key={i} className="mx-8 text-2xl font-light lg:text-3xl text-white tracking-wide">{item}</span>
@@ -22,39 +27,33 @@ export default function ServicesSection() {
 					</div>
 				</div>
 			</div>
-			<div className="relative h-full w-full grid grid-flow-col grid-rows-3">
+			<div className="row-start-3 relative h-full w-full grid grid-flow-col grid-rows-[30%_70%]">
 				<SectionTitle title="SERVICIOS" className="place-self-center" />
-				<div className="place-self-start w-full flex flex-col gap-3 text-center font-normal text-white text-[11px]/[12px] md:text-[11px]/[14px] 2xl:text-[13px]/[16px] tracking-widest place-self-center">
-					<div className="flex flex-col gap-3">
+				<div className="place-self-start w-full flex flex-col gap-3 text-center font-normal text-white text-[11px]/[12px] md:text-[11px]/[14px] xl:text-xs tracking-widest place-self-center">
+					<div className="flex flex-col gap-5 xl:gap-6">
 						<div className="grid grid-rows-2 grid-cols-3">
 							<p className="col-span-3">PILATES MÁQUINA</p>
-							<div className="col-start-2 col-span-2 h-[1px] mt-1 bg-white"></div>
+							<div className="col-start-2 col-span-2 h-[1px] lg:h-[2px] mt-1 bg-white"></div>
 						</div>
 						<div className="grid grid-rows-2 grid-cols-3">
 							<p className="col-span-3">PILATES EMBARAZO</p>
-							<div className="col-span-2 h-[1px] mt-1 bg-white"></div>
+							<div className="col-span-2 h-[1px] lg:h-[2px] mt-1 bg-white"></div>
 						</div>
-					</div>
-				</div>
-				<div className="place-self-start w-full flex flex-col gap-3 text-center font-normal text-white text-[11px]/[12px] md:text-[11px]/[14px] 2xl:text-[13px]/[16px] tracking-widest place-self-center">
-
-					<div className="flex flex-col gap-3">
 						<div className="grid grid-rows-2 grid-cols-3">
 							<p className="col-start-2 place-self-center items-center">ENTRENAMIENTO PARA BAILARINES</p>
-							<div className="row-start-2 col-start-2 col-span-2 h-[1px] mt-1 bg-white"></div>
+							<div className="row-start-2 col-start-2 col-span-2 h-[1px] lg:h-[2px] mt-1 bg-white"></div>
 						</div>
 						<div className="grid grid-rows-2 grid-cols-3">
 							<p className="col-span-3">CLASES PRIVADAS</p>
-							<div className="col-span-2 h-[1px] mt-1 bg-white"></div>
+							<div className="col-span-2 h-[1px] lg:h-[2px] mt-1 bg-white"></div>
 						</div>
 					</div>
 				</div>
 			</div>
-			<div className="pt-20 pl-10 lg:pl-50">
+			{/* <div className="pt-20 pl-10 lg:pl-50">
 				<p className="font-bold text-white text-[11px]/[12px] md:text-[13px]/[14px] 2xl:text-[16px]/[16px] tracking-widest">CLASES DE <span className="font-light text-2xl md:text-4xl">50</span> MIN</p>
 				<p className="font-light text-white text-[8px]/[5px] md:text-[10px]/[6px] 2xl:text-[12px]/[7px] tracking-[.65em]">MAÑANA Y TARDE</p>
-			</div>
-			<MainAcero imageLayout="absolute bottom-0 right-1/2 transform translate-x-1/2" />
-		</div>
+			</div> */}
+		</section>
 	);
 }
