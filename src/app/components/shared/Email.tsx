a92319b7bda8b9b04b4841d2b2811b2acb59b26a
MargusS <agusnicolas21@gmail.com>
2025-10-10 21:08:32 +0100

    Add typing animatiton

--- src/app/components/shared/Email.tsx
+++ src/app/components/shared/Email.tsx
@@ -1,8 +1,9 @@
+import TextType from '@/app/animation/TextType';
 import Image from 'next/image';
 export default function Email({ className }: { className?: string }) {
 
 	return (
-		<div className={`${className} flex items-center gap-1 text-white text-[8px]/[12px] `}>
+		<div className={`${className} flex items-center gap-1 text-white text-[8px]/[12px] h-[25px]`}>
 			<Image
 				src="/icons/alt.svg"
 				alt="Instagram"
@@ -10,10 +11,24 @@ export default function Email({ className }: { className?: string }) {
 				height={20}
 				className="md:w-[25px] 2xl:w-[30px]"
 			/>
-			<div className='flex flex-col tracking-widest text-[12px]/[13px] md:text-xs/[14px] 2xl:text-base/[18px]'>
+			<div className='flex flex-col tracking-widest text-[12px]/[13px] md:text-xs/[14px] 2xl:text-base/[18px] w-[100px]  2xl:w-[140px]'>
+				<TextType
+					text={["aceropilatesstudio gmail.com", "aceropilatesstudio gmail.com"]}
+					typingSpeed={75}
+					pauseDuration={1500}
+					showCursor={true}
+					cursorCharacter="|"
+					cursorBlinkDuration={500}
+					className="font-normal tracking-[1em]"
+					showCursor={false}
+				/>
+				
+
+			</div>
+			{/* <div className='flex flex-col tracking-widest text-[12px]/[13px] md:text-xs/[14px] 2xl:text-base/[18px]'>
 				<span className="font-bold">aceropilatesstudio</span>
 				<span className="font-light  tracking-[.82em]">gmail.com</span>
-			</div>
+			</div> */}
 		</div>
 	);
 }
